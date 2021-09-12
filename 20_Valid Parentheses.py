862f5f6a82655da51f2d26971ce81a70711b07e1
Qihua Gong <gongqihua0115@gmail.com>
2021-09-11 21:53:52 -0400

    python solution

--- 20_Valid Parentheses.py
+++ 20_Valid Parentheses.py
@@ -1,8 +1,9 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = []
-        mapping = {"(" : ")", "{" : "}", "[" : "]"}
-        
+        stack = [] #创建一个删除式list
+        mapping = {"(" : ")", "{" : "}", "[" : "]"} #对照表dict
+        #思路：将前一半字符放入对照表，对应另一半字符，遇到前一半字符，则将对应
+        #     的另一半存入空list中，遇后一半字符则检测list中前一位是否与它相同
         for item in s:
             if item in mapping.keys():
                 stack.append(mapping[item])
