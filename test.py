92ba8b2a7c01438f711a3961fe8f941688db8ce7
Qihua Gong <gongqihua0115@gmail.com>
2021-10-21 13:14:34 -0400

    test

---
+++ test.py
@@ -0,0 +1,14 @@
+def twoSum(nums, target):
+        
+    dict = {}
+        
+    for i in range(len(nums)):
+        if target - nums[i] not in dict:
+            dict[nums[i]] = i
+        else:
+            return[dict[target - nums[i]] , i]
+
+nums = [3,2,4]
+target = 6
+output = twoSum(nums, target)
+print(output)
