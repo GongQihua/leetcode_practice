a3b6fab76cdc60cef2bdb662907a0c7545d75c15
Qihua Gong <gongqihua0115@gmail.com>
2022-01-17 13:44:36 -0500

    python

---
+++ Algorithms_medium/38_Count and Say.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        s = '1'
+        for _ in range(n-1):
+            i = 0
+            t = []
+            while i < len(s):
+                j = i
+                while j < len(s) and s[j] == s[i]:
+                    j += 1
+                t.append(str(j - i))
+                t.append(str(s[i]))
+                i = j
+            s = ''.join(t)
+        return s
+'''
+垃圾题目，方法是设计一个计数器一个添加元素，对着添加，一个1两个1这种
+'''
