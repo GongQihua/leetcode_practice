81d8ece63b383f09d5d9173d9669a6b3f65d716f
QihuaGong <gongqihua0115@gmail.com>
2022-01-25 20:14:35 -0500

    python

---
+++ Algorithms_medium/47_Permutations II.py
@@ -0,0 +1,25 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        res = []
+        path = [0] * n
+        used = [False] * n
+        nums.sort()
+
+        def dfs(u):
+            if u == n:
+                res.append(path.copy())
+                return
+            for i in range(n):
+                if used[i] or (i > 0 and nums[i] == nums[i - 1] and not used[i - 1]):
+                    continue
+                path[u] = nums[i]
+                used[i] = True
+                dfs(u + 1)
+                used[i] = False
+
+        dfs(0)
+        return res
+'''
+dfs深度搜索，于上一道题一样，区别是记得首先整理数组，然后dfs的时候检查是否有重复
+'''
