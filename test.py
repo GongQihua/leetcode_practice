1ce3537e8580d135c823bd65df189bba1084a79a
QihuaGong <gongqihua0115@gmail.com>
2022-04-13 15:11:16 -0400

    aa

--- test.py
+++ test.py
@@ -1,14 +1,21 @@
-def twoSum(nums, target):
-        
-    dict = {}
-        
-    for i in range(len(nums)):
-        if target - nums[i] not in dict:
-            dict[nums[i]] = i
-        else:
-            return[dict[target - nums[i]] , i]
+def func(input):
 
-nums = [2,4,6]
-target = 8
-output = twoSum(nums, target)
-print(output)
+    # please define the python3 input here. For example: a,b = map(int, input().strip().split())
+    # please finish the function body here.
+    # please define the python3 output here. For example: print().
+    n = len(input)
+    word, count = 1, 0
+    for i in range(0, n):
+        if input[i] == ' ':
+            word += 1
+            continue
+        else:
+            count += 1
+    print('word = ',word)
+    print('count = ', count)
+    output = count / word
+    print('%.2f' %output)
+
+if __name__ == "__main__":
+    input = 'Who Love Solo'
+    func(input)
