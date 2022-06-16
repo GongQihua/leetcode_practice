aecde5328f80ff253adc5fa109ad28b4784c14d5
QihuaGong <gongqihua0115@gmail.com>
2022-06-16 14:59:11 -0400

    python

--- Algorithms_medium/114_Flatten Binary Tree to Linked List.py
+++ Algorithms_medium/114_Flatten Binary Tree to Linked List.py
@@ -22,4 +22,7 @@ class Solution:
 '''
 因为要满足复杂度的原因，思路是把原root的右节点换到原左节点右分支的最后，然后把原左节点带着树移到右节点
 如此反复就能得到一个只有右节点的树
+以题目给的[1,2,5,3,4,null,6]为例子，这是变换过程的输出结果：
+root: TreeNode{val: 1, left: None, right: TreeNode{val: 2, left: TreeNode{val: 3, left: None, right: None}, right: TreeNode{val: 4, left: None, right: TreeNode{val: 5, left: None, right: TreeNode{val: 6, left: None, right: None}}}}}
+root: TreeNode{val: 2, left: None, right: TreeNode{val: 3, left: None, right: TreeNode{val: 4, left: None, right: TreeNode{val: 5, left: None, right: TreeNode{val: 6, left: None, right: None}}}}}
 '''
