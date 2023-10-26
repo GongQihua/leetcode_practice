440a2d090f242a1f892e7ba594d8e4d497fcc61a
GongQihua <gongqihua0115@gmail.com>
2023-10-26 14:29:12 +0800

    python

---
+++ Algorithms_python/1_Two_Sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashtable = dict()
+        for i, num in enumerate(nums):
+            if target - num in hashtable:
+                return [hashtable[target - num], i]
+            hashtable[nums[i]] = i
+        return []
+
+#入门基础，两种解法，顺序搜索和哈希表，这里是哈希表。
