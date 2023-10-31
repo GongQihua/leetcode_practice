d09386fa41a94c5e4fde007d4e3293e656d442d9
Qihua Gong <gongqihua0115@gmail.com>
2023-10-31 16:28:42 +0800

    python

--- Algorithms_python/7_Reverse_Integer.py
+++ Algorithms_python/7_Reverse_Integer.py
@@ -8,9 +8,13 @@ class Solution:
             num = num * 10 + temp
             a = int(a/10)
             
-        if x > 0 and num < 2147483647:
+        if x > 0 and num < 2147483647: #2**31，最大32位
             return num
         elif x < 0 and num <= 2147483647:
             return -num
         else:
             return 0
+        
+'''
+easy题，直接做成字符串反转也行
+'''
