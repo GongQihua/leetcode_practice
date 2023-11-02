c2c9aa1ccff921ea39dfc47be4c11f146e3d79d1
GongQihua <gongqihua0115@gmail.com>
2023-11-02 10:00:56 +0800

    python

--- Algorithms_python/12_Integer to Roman.py
+++ Algorithms_python/12_Integer to Roman.py
@@ -1,12 +1,29 @@
 class Solution:
+    VALUE_SYMBOLS = [
+        (1000, "M"),
+        (900, "CM"),
+        (500, "D"),
+        (400, "CD"),
+        (100, "C"),
+        (90, "XC"),
+        (50, "L"),
+        (40, "XL"),
+        (10, "X"),
+        (9, "IX"),
+        (5, "V"),
+        (4, "IV"),
+        (1, "I"),
+    ]
     def intToRoman(self, num: int) -> str:
-        nums = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
         roman = []
-        for a,b in nums:
-            while num >= a:
-                num = num -a
-                roman.append(b)
-        return "".join(roman)
+        for value, symbol in Solution.VALUE_SYMBOLS:
+            while num >= value:
+                num -= value
+                roman.append(symbol)
+            if num == 0:
+                break
+        return ''.join(roman)
 '''
 创一个字典，然后导入原数字，从大往小减，每减一次，保存减的罗马数字，组成新字符串
+因为4和9的特殊性，记得直接把4，9，40，90，400，900的情况写进dic里
 '''
