45ed1fcb7af1e2d9b1b794b7b3857cb1044dcc10
GongQihua <gongqihua0115@gmail.com>
2024-06-14 15:52:30 +0800

    update 6.14

---
+++ Algorithms_C#/25_Reverse_Nodes_in_k-Group.cs
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode ReverseKGroup(ListNode head, int k) {
+        ListNode hair = new ListNode(0);
+        hair.next = head;
+        ListNode pre = hair;
+
+        while (head != null) {
+            ListNode tail = pre;
+            for (int i = 0; i < k; ++i) {
+                tail = tail.next;
+                if (tail == null) {
+                    return hair.next;
+                }
+            }
+            ListNode nex = tail.next;
+            ListNode[] reverse = myReverse(head, tail);
+            head = reverse[0];
+            tail = reverse[1];
+            pre.next = head;
+            tail.next = nex;
+            pre = tail;
+            head = tail.next;
+        }
+
+        return hair.next;
+    }
+
+    public ListNode[] myReverse(ListNode head, ListNode tail) {
+        ListNode prev = tail.next;
+        ListNode p = head;
+        while (prev != tail) {
+            ListNode nex = p.next;
+            p.next = prev;
+            prev = p;
+            p = nex;
+        }
+        return new ListNode[]{tail, head};
+    }
+
+}
---
+++ Algorithms_C#/27_Remove_Element.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        int k = 0;
+        foreach(int x in nums){
+            if(x != val){
+                nums[k] = x;
+                ++k;
+            }
+        }
+        return k;
+    }
+}
