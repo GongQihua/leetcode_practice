2641b79b2ff9d57b2274532f0d1a284ac774ba16
Qihua Gong <gongqihua0115@gmail.com>
2025-03-13 11:41:13 +0800

    test1

--- test1.py
+++ test1.py
@@ -1,39 +1,22 @@
-def enhanced_strstr(haystack, needle):
-    haystack_len = len(haystack)
-    needle_len = len(needle)
- 
-    for i in range(haystack_len):
-        j = 0
-        k = i
-        match = True
- 
-        while j < needle_len and k < haystack_len:
-            if needle[j] == '[':  # 开始处理可选段
-                option_matched = False
-                j += 1  # 移动到 '[' 后面的第一个字符
-                while needle[j] != ']':
-                    if haystack[k] == needle[j]:
-                        option_matched = True
-                    j += 1  # 移动到下一个可选字符
-                if not option_matched:
-                    match = False
-                    break
-                j += 1  # 跳过 ']'
-            else:  # 处理普通字符
-                if haystack[k] != needle[j]:
-                    match = False
-                    break
-                j += 1  # 移动到 needle 的下一个字符
-            k += 1  # 移动到 haystack 的下一个字符
- 
-        if match:
-            return i  # 返回匹配的起始位置
- 
-    return -1  # 没有匹配
- 
-if __name__ == "__main__":
-    haystack = input().strip()
-    needle = input().strip()
-    
-    result = enhanced_strstr(haystack, needle)
-    print(result)
+def is_prime(n):
+    return n > 1 and all(n % d for d in range(2, int(n**.5) + 1))
+
+def reverse(x):
+    ans = 0
+    while x:
+        ans = 10 * ans + x % 10
+        x /= 10
+    return ans
+
+def main():
+    N  = int(input())
+    while True:
+        if N == reverse(N) and is_prime(N):
+            return N
+        N += 1
+        if 10**7 < N < 10**8:
+            N = 10**8
+
+if __name__ == '__main__':
+    ans = main()
+    print(ans)
