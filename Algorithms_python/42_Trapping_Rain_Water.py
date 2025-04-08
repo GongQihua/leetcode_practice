5bc34ecb0a4b532934d099677d7df04908abb925
Qihua Gong <gongqihua0115@gmail.com>
2025-04-08 09:20:32 +0800

    Update 42_Trapping_Rain_Water.py

--- Algorithms_python/42_Trapping_Rain_Water.py
+++ Algorithms_python/42_Trapping_Rain_Water.py
@@ -11,6 +11,23 @@ class Solution:
             rightmax[i] = max(rightmax[i + 1], height[i])
         ans = sum(min(leftmax[i], rightmax[i]) - height[i] for i in range(n))
         return ans
+    
+    def trap2(self, height: List[int]) -> int: # 基于上一种方法的优化，双指针一遍遍历，思路是一样的
+        ans = 0
+        left, right = 0, len(height) - 1
+        leftmax = rightmax = 0
+
+        while left < right:
+            leftmax = max(leftmax, height[left])
+            rightmax = max(rightmax, height[right])
+            if height[left] < height[right]:
+                ans += leftmax - height[left]
+                left += 1
+            else:
+                ans += rightmax - height[right]
+                right -= 1
+                
+        return ans
 '''
 正向遍历找leftmax的每一个值，再反向遍历找rightmax的每一个值，
 然后用小的那个值减现有值，再累加得到解。
