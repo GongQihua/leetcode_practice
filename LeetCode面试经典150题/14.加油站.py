1b426fc1c0d5b53320b22036b696f063593cbc35
gongqihua <gongqihua0115@gmail.com>
2026-03-28 10:57:46 +0800

    update_0328

---
+++ LeetCode面试经典150题/14.加油站.py
@@ -0,0 +1,19 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        n = len(gas)
+        i = 0
+        while i < n:
+            sum_gas = sum_cost = 0
+            cnt = 0
+            while cnt < n:
+                j = (i + cnt) % n
+                sum_gas += gas[j]
+                sum_cost += cost[j]
+                if sum_cost > sum_gas:
+                    break
+                cnt += 1
+            if cnt == n:
+                return i
+            else:
+                i += cnt + 1
+        return -1
