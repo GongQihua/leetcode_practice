d51588917df5cf7b83092493bfda369d3231fcec
gongqihua <gongqihua0115@gmail.com>
2026-05-22 09:45:33 +0800

    update_0522

--- LeetCode面试经典150题/2.移除元素.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        n = len(nums)
-        left = 0
-        for right in range(n):
-            if nums[right] != val:
-                nums[left] = nums[right]
-                left += 1
-        return left
--- LeetCode面试经典150题/8.买卖股票的最佳时机 II.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        ans = 0
-        n = len(prices)
-        for i in range(1, n):
-            ans += max(0, prices[i] - prices[i - 1])
-        return ans
